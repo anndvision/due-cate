e355c32ddb25193dc45d50b04055597e432c35af
Andrew Jesson <andrew.jesson@cs.ox.ac.uk>
2021-05-23 13:42:17 +0100

    added library

---
+++ due_cate/library/datasets/__init__.py
@@ -0,0 +1,8 @@
+from due_cate.library.datasets.ihdp import IHDP
+
+from due_cate.library.datasets.ihdp_cov import IHDPCov
+
+DATASETS = {
+    "ihdp": IHDP,
+    "ihdp-cov": IHDPCov,
+}
---
+++ due_cate/library/models/__init__.py
@@ -0,0 +1,2 @@
+from due_cate.library.models.tarnet import TARNet
+from due_cate.library.models.deep_kernel import DeepKernelGP
